4330bb7a17fc59779e4204ebffd56432d552bbce
Kevin Mayfield <KevinMayfield@users.noreply.github.com>
2024-02-23 08:45:54 +0000

    HAPI FHIR 7.0.0 and Spring 3 Conversion

--- src/main/kotlin/uk/nhs/england/fhirvalidator/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/uk/nhs/england/fhirvalidator/configuration/SecurityConfiguration.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.http.HttpHeaders
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
@@ -25,7 +26,7 @@ open class SecurityConfiguration  {
                 csrf
                     .disable()
             }
-            .cors{ cors -> corsConfigurationSource()};
+            .cors{ cors -> corsConfigurationSource()}
         return http.build()
     }
 
@@ -33,13 +34,20 @@ open class SecurityConfiguration  {
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
         configuration.addAllowedMethod("GET")
-        configuration.addAllowedMethod("PATCH")
+        configuration.addAllowedMethod("PUT")
         configuration.addAllowedMethod("PUT")
         configuration.addAllowedMethod("POST")
         configuration.addAllowedMethod("OPTIONS")
         configuration.allowedOrigins = Arrays.asList("*")
-        configuration.allowedMethods = Arrays.asList("*")
+        //configuration.allowedMethods = Arrays.asList("*")
         configuration.allowedHeaders = Arrays.asList("*")
+        configuration.maxAge = 3600
+        configuration.exposedHeaders = Arrays.asList(
+            HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
+            HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS,
+            HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS,
+            HttpHeaders.ACCESS_CONTROL_MAX_AGE,
+            HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS)
         val source = UrlBasedCorsConfigurationSource()
         source.registerCorsConfiguration("/**", configuration)
         return source
